d60ab5f721acde670f17e4c6f7909756c173ebe9
krishna singh <153911304+Kri01ceram@users.noreply.github.com>
2025-08-21 19:57:32 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from scipy.stats import chi2_contingency
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, precision_recall_fscore_support
+import warnings
+import os
+
+
+a1 = pd.read_excel("D:\\Credit-Risk-DEtection\\case_study1.xlsx")
+a2 = pd.read_excel("D:\\Credit-Risk-DEtection\\case_study2.xlsx")
+
+
+df1 = a1.copy()
+df2 = a2.copy()
+
