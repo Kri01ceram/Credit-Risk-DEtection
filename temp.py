d2ff33431b7f46c2a11f5a572b320e4a6cd3b0e0
krishna singh <153911304+Kri01ceram@users.noreply.github.com>
2025-08-21 20:01:53 +0530

    Add files via upload

--- temp.py
+++ temp.py
@@ -25,3 +25,16 @@ a2 = pd.read_excel("D:\\Credit-Risk-DEtection\\case_study2.xlsx")
 df1 = a1.copy()
 df2 = a2.copy()
 
+
+
+ #removing null values
+df1 = df1.loc[df1['Age_Oldest_TL'] != -99999]
+
+columns_to_be_removed = []
+
+for i in df2.columns:
+    if df2.loc[df2[i] == -99999].shape[0] > 10000:
+        columns_to_be_removed .append(i)
+        
+        
+df2=df2.drop(columns_to_be_removed,axis=1)
